a593396a7ff4c2dba36ede7aec9890d90e456284
Shamsul Hasan Siam <shamsulhasansiam@gmail.com>
2019-09-09 20:38:48 +0600

    Assignment added

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name="";
+            string val;
+            
+            string address ="";
+             string contanct ="";
+             Console.WriteLine("Please Enter Your Name: ");
+             name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            val = Console.ReadLine();
+
+            int age = Convert.ToInt32(val);
+      
+             Console.WriteLine("Please Enter Your address: ");
+             address = Console.ReadLine();
+             Console.WriteLine("Please Enter Your contact: ");
+             contanct = Console.ReadLine();
+             Console.WriteLine("Name: " + name);
+             Console.WriteLine("Your age is: {0}", age);
+             Console.WriteLine("Address: " + address);
+             Console.WriteLine("Contact no: " + contanct);
+             Console.ReadKey();
+             
+        }
+    }
+}
