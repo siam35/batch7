904ce45d63cfa2386dec0b1009cd8a93f46accee
spark01 <btapas28@gmail.com>
2019-09-12 15:53:44 +0600

    windowsapp added

---
+++ WindowsFormsApp1/WindowsFormsApp1/Home.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ShowButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.shownameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(668, 79);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowButton.TabIndex = 1;
+            this.ShowButton.Text = "show";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(344, 79);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(42, 20);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // shownameLabel
+            // 
+            this.shownameLabel.AutoSize = true;
+            this.shownameLabel.Location = new System.Drawing.Point(674, 182);
+            this.shownameLabel.Name = "shownameLabel";
+            this.shownameLabel.Size = new System.Drawing.Size(0, 13);
+            this.shownameLabel.TabIndex = 3;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.shownameLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.ShowButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ShowButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label shownameLabel;
+    }
+}
+
